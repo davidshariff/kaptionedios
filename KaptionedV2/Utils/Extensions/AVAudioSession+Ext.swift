cbdc6c3509bb98c663ed0f116ad0ef95a3cb7605
David <david@web3platforms.com>
2025-08-16 16:53:45 +0800

    rename unused audio

--- KaptionedV2/Utils/Extensions/AVAudioSession+Ext.swift
+++ KaptionedV2/Utils/Extensions/AVAudioSession+Ext.swift
@@ -1,38 +1,6 @@
-//
-//  AVAudioSession+Ext.swift
-//  VideoEditorSwiftUI
-//
-//  Created by Bogdan Zykov on 04.05.2023.
-//
-
 import AVFoundation
 
-extension AVAudioSession{
-    
-    
-    func playAndRecord(){
-        print("Configuring playAndRecord session")
-        do {
-            try self.setCategory(.playAndRecord, mode: .default)
-            try self.overrideOutputAudioPort(AVAudioSession.PortOverride.none)
-            print("AVAudio Session out options: ", self.currentRoute)
-          print("Successfully configured audio session.")
-        } catch (let error) {
-          print("Error while configuring audio session: \(error)")
-        }
-    }
-    
-    func configureRecordAudioSessionCategory() {
-      print("Configuring record session")
-      do {
-          try self.setCategory(.record, mode: .default)
-          try self.overrideOutputAudioPort(AVAudioSession.PortOverride.none)
-          print("AVAudio Session out options: ", self.currentRoute)
-        print("Successfully configured audio session.")
-      } catch (let error) {
-        print("Error while configuring audio session: \(error)")
-      }
-    }
+extension AVAudioSession {
     
     func configurePlaybackSession(){
         print("Configuring playback session")
@@ -45,4 +13,5 @@ extension AVAudioSession{
             print("#configureAudioSessionToSpeaker Error \(error.localizedDescription)")
         }
     }
+
 }
